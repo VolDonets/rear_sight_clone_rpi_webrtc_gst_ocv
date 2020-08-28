5c49bd1fe8b40ca929a6be8e3c2c1c355d88b8c0
VolDonets <vovan.s.marsa@gmail.com>
2020-08-28 19:34:05 +0300

    Modified a FaceDetectionProcessor

--- face_detector_ocv/face_detection_processor.cpp
+++ face_detector_ocv/face_detection_processor.cpp
@@ -3,3 +3,43 @@
 //
 
 #include "face_detection_processor.h"
+
+FaceDetectionProcessor::FaceDetectionProcessor() {
+    mutexProc.lock();
+}
+
+FaceDetectionProcessor::~FaceDetectionProcessor() {
+
+}
+
+void FaceDetectionProcessor::add_frame(cv::Mat frame) {
+    mutexRes.lock();
+    if (queueFrames.size() > MAX_MATS_LIST_SIZE) {
+        queueFrames.pop_front();
+        queueFrames.push_back(frame);
+    } else
+        queueFrames.push_back(frame);
+    mutexProc.unlock();
+    mutexRes.unlock();
+}
+
+void FaceDetectionProcessor::processRecognition() {
+    recognitionProcessThread = std::thread([this](){
+        while (true) {
+            mutexProc.lock();
+            mutexRes.lock();
+            cv::Mat currentFrame, greyFrame;
+            currentFrame = queueFrames.front();
+            queueFrames.pop_front();
+            mutexRes.unlock();
+            cv::cvtColor(currentFrame, greyFrame, cv::COLOR_BGR2GRAY);
+            cv::resize(greyFrame, currentFrame, cv::Size(), 1/SCALING_IMG, 1/SCALING_IMG, cv::INTER_LINEAR);
+            cv::equalizeHist(currentFrame, currentFrame);
+            cascadeClassifier.detectMultiScale(smallIm)
+        }
+    });
+}
+
+std::vector<cv::Rect> *FaceDetectionProcessor::getLastDetectedFaces() {
+    return nullptr;
+}
--- face_detector_ocv/face_detection_processor.h
+++ face_detector_ocv/face_detection_processor.h
@@ -5,21 +5,31 @@
 #ifndef REAR_SIGHT_CLONE_PROJECT_FACE_DETECTION_PROCESSOR_H
 #define REAR_SIGHT_CLONE_PROJECT_FACE_DETECTION_PROCESSOR_H
 
-#define MAX_MATS_LIST_SIZE                  3;
+#define MAX_MATS_LIST_SIZE                  3
+#define SCALING_IMG                         1
 
 #include <list>
 #include <opencv2/core/mat.hpp>
+#include <opencv2/opencv.hpp>
+#include <thread>
 
 class FaceDetectionProcessor {
 private:
     std::list<cv::Mat> queueFrames;
+    std::vector<cv::Rect> *facesCoords ;
+    cv::CascadeClassifier cascadeClassifier;
+    std::thread recognitionProcessThread;
+
+    std::mutex mutexRes;
+    std::mutex mutexProc;
+
 public:
     FaceDetectionProcessor();
     ~FaceDetectionProcessor();
 
     void add_frame(cv::Mat frame);
     void processRecognition();
-    void /*not void I'll patch it*/ getLastDetectedFaces();
+    std::vector<cv::Rect> *getLastDetectedFaces();
 };
 
 
